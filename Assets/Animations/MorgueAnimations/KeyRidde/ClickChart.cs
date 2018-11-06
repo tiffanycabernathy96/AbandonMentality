4b317c070bd73d186f187c995508c56817217591
Kyle Gabriel <kgabriel@email.sc.edu>
2018-11-06 16:26:20 -0500

    Made chart in the morgue clickable

---
+++ Assets/Animations/MorgueAnimations/KeyRidde/ClickChart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickChart : Puzzle {
+
+    public Image image;
+	// Use this for initialization
+	void Start () {
+        image.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayCastHit;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out rayCastHit, Mathf.Infinity))
+            {
+                //clicked on the chart
+                ClickChart chart = rayCastHit.transform.GetComponent<ClickChart>();
+                if(chart)
+                {
+                    image.enabled = !image.enabled;
+                }
+                
+
+            }
+           
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            image.enabled = false;
+        }
+        
+	}
+
+    
+}
